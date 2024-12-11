3569d591ffaa20e5ee645825ef68673d0fcf1d17
roulaimad <roulaimad7@gmail.com>
2024-12-11 21:10:37 +0100

    handling positions selection

--- src/Components/PositionBoxes.jsx
+++ src/Components/PositionBoxes.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PositionBox from "./PositionBox";
 
 function PositionBoxes() {
+  const positionsList = ["ROCK", "PAPER", "SCISSORS"];
+
+  const [position, setPosition] = useState({
+    playerPosition: "",
+    computerPosition: "",
+  });
+
+  const handlePositionClick = (selectedPosition) => {
+    setPosition({
+      playerPosition: selectedPosition,
+      computerPosition: positionsList[Math.floor(Math.random() * 3)],
+    });
+  };
+
+  useEffect(() => {
+    console.log("Player Position:", position.playerPosition);
+    console.log("Computer Position:", position.computerPosition);
+    console.log("----------------------------");
+  }, [position]);
   const boxes = [
     {
       bgColor: "#211f4f",
@@ -27,7 +46,11 @@ function PositionBoxes() {
     <div className="flex items-center justify-center mb-40">
       <div className="grid grid-cols-3 gap-4  ">
         {boxes.map((box, index) => (
-          <PositionBox key={index} box={box} />
+          <PositionBox
+            key={index}
+            box={box}
+            handlePositionClick={handlePositionClick}
+          />
         ))}
       </div>
     </div>
